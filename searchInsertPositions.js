c20f5de30bc3ebaa6350936e728953dae80b9d28
Aswini-mca <135096729+Aswini-mca@users.noreply.github.com>
2023-06-13 15:56:10 +0530

    Update searchInsertPositions.js

--- searchInsertPositions.js
+++ searchInsertPositions.js
@@ -1,15 +1,15 @@
-let nums=[1,3,5,6]
-let target=5
 function searchTarget(nums,target){
     for(let i=0;i<nums.length;i++){
     if(nums[i]==target){
         return i
     } 
-    else if(nums[i]<target)
+}
+for(let i=0;i<nums.length;i++){
+    if(nums[i]>target)
     {
         return i
     }
-    }
-        return nums.length
+} 
+    
 }
-console.log(searchTarget(nums,target))
+console.log(searchTarget([1,3,5,6],5))
